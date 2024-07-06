68bc1d7e2528d0f69a1bb7cb676ee8a8e24c57dd
balar2207 <135403084+balar2207@users.noreply.github.com>
2024-07-06 08:27:31 +0530

    Update task2a.sh

--- task2a.sh
+++ task2a.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #To scan files under "/" and find files that are greater than 5MB and generate a report.
-find / -size +5M -type f -executable -not -path '*/root/*' -exec du -h {} \; | sort -n > /root/Shell-Scripting-Class/DevOps_Class/reports/5MB_report.txt
+find / -size +5M -type f -executable -not -path '*/root/*' -exec du -h {} \; | sort -n > /root/DevOps_Class/reports/5MB_report.txt
 
 
 echo "The list is saved under /root/Shell-Scripting-Class/DevOps_Class/reports"
